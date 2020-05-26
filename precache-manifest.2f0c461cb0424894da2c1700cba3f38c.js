78c7bfbae23c4649d465d62d5e42c70bd5b59270
Mayank Sharma <mayanksharma78613@gmail.com>
2020-05-26 23:31:59 +0530

    Updates

---
+++ precache-manifest.2f0c461cb0424894da2c1700cba3f38c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c2741885cb416f90ac4e6e7beb1489",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "4e9581d483d96b8fb5d7",
+    "url": "/myportfolio/static/css/main.8e8d71c2.chunk.css"
+  },
+  {
+    "revision": "485a0f06ae9b6ba3ed8b",
+    "url": "/myportfolio/static/js/2.d95c05f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myportfolio/static/js/2.d95c05f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9581d483d96b8fb5d7",
+    "url": "/myportfolio/static/js/main.0c8fd9f9.chunk.js"
+  },
+  {
+    "revision": "bc6701cc98be9f46d7ad",
+    "url": "/myportfolio/static/js/runtime-main.699ab192.js"
+  },
+  {
+    "revision": "a64bec05670f0a1de0b58e3f6e5da242",
+    "url": "/myportfolio/static/media/fb.a64bec05.png"
+  },
+  {
+    "revision": "58452ba59cf6ca08e053e864a132a34d",
+    "url": "/myportfolio/static/media/gokartscr1.58452ba5.png"
+  },
+  {
+    "revision": "50f3664e2869fe207a88c08f75879c34",
+    "url": "/myportfolio/static/media/home.50f3664e.png"
+  },
+  {
+    "revision": "bf251c2242710d76796aab0fc8096219",
+    "url": "/myportfolio/static/media/homepage-compressed.bf251c22.jpg"
+  },
+  {
+    "revision": "008c52dd4229e3f279974aa75a7d8083",
+    "url": "/myportfolio/static/media/image3.008c52dd.png"
+  },
+  {
+    "revision": "d2876bc05f204f4f1e70373458f2ca2b",
+    "url": "/myportfolio/static/media/lyfscr.d2876bc0.png"
+  },
+  {
+    "revision": "1be512a01eb16574d153d846bed99553",
+    "url": "/myportfolio/static/media/port.1be512a0.jpg"
+  },
+  {
+    "revision": "4a5515fc7a80237c57b05c19bc75d978",
+    "url": "/myportfolio/static/media/portfolio.4a5515fc.jpg"
+  },
+  {
+    "revision": "cd3b420e4778f81aad1ce122365da4bf",
+    "url": "/myportfolio/static/media/wave2.cd3b420e.png"
+  }
+]);
